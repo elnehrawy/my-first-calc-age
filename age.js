863cb66f246a071de0e05b4a311659918c3f38b7
Elnehrawy <truth.voice2008@gmail.com>
2018-09-13 16:43:25 +0200

    some fix to js spaces

--- age.js
+++ age.js
@@ -11,7 +11,7 @@ function age() {
     //date now 
         today = new Date(),
         dd = today.getDate(),
-        mm = today.getMonth()+1, //January is 0!
+        mm = today.getMonth() + 1, //January is 0!
         yyyy = today.getFullYear(),
     //date now - my age    
         resultDay = dd - parseInt(calcDay),
@@ -27,40 +27,36 @@ function age() {
         resultYearNegative = Math.abs(resultYear - 1);
      // if for day and month <= 0     
     if (resultDay <= 0 && resultMonth <= 0 && resultYear) {
-        year.innerHTML = "You have <mark> " + resultYearNegative + " </mark>years",
-        month.innerHTML = " and <mark> " + resultMonthNegative + " </mark>months",
-        day.innerHTML = " and <mark> " + resultDayNegative + " </mark>days.<br><ins>&copy; 2008-2018 EL-NEHRAWY front-end web designer.All Rights Reserved.</ins>";     
-                    
-        }
+        year.innerHTML = "You have <mark> " + resultYearNegative + " </mark>years";
+        month.innerHTML = " and <mark> " + resultMonthNegative + " </mark>months";
+        day.innerHTML = " and <mark> " + resultDayNegative + " </mark>days.<br><ins>&copy; 2008-2018 EL-NEHRAWY front-end web designer.All Rights Reserved.</ins>"; 
+    }
     // if for day only <= 0
-    else if (resultDay <= 0 && resultMonth && resultYear){
-        year.innerHTML = "You have <mark> " + resultYear + " </mark>years",
-        month.innerHTML = " and <mark> " + resultMonthNegative + " </mark>months",
+        else if (resultDay <= 0 && resultMonth && resultYear) {
+        year.innerHTML = "You have <mark> " + resultYear + " </mark>years";
+        month.innerHTML = " and <mark> " + resultMonthNegative + " </mark>months";
         day.innerHTML = " and <mark> " + resultDayNegative + " </mark>days.<br><ins>&copy; 2008-2018 EL-NEHRAWY front-end web designer.All Rights Reserved.</ins>";     
-             }
+    }
     // if for month only <= 0
-    else if (resultMonth <= 0 && resultYear && resultDay){
-        year.innerHTML = "You have <mark> " + resultYearNegative + " </mark>years",
-        month.innerHTML = " and <mark> " + parseInt(resultMonth + 12) + " </mark>months",    
+        else if (resultMonth <= 0 && resultYear && resultDay) {
+        year.innerHTML = "You have <mark> " + resultYearNegative + " </mark>years";
+        month.innerHTML = " and <mark> " + parseInt(resultMonth + 12) + " </mark>months";    
         day.innerHTML = " and <mark> " + resultDay + " </mark>days.<br><ins>&copy; 2008-2018 EL-NEHRAWY front-end web designer.All Rights Reserved.</ins>";
-             }
+        }
     // if for day and month and year >= 0
-    else if (resultDay >= 0 && resultMonth >= 0 && resultYear >= 0) {
-        year.innerHTML = "You have <mark> " + resultYear + " </mark>years",
-        month.innerHTML = " and <mark> " + resultMonth + " </mark>months",    
+        else if (resultDay >= 0 && resultMonth >= 0 && resultYear >= 0) {
+        year.innerHTML = "You have <mark> " + resultYear + " </mark>years";
+        month.innerHTML = " and <mark> " + resultMonth + " </mark>months";    
         day.innerHTML = " and <mark> " + resultDay + " </mark>days.<br><ins>&copy; 2008-2018 EL-NEHRAWY front-end web designer.All Rights Reserved.</ins>";
     }
     // if for year only == 0 and day <=0 (year == 0)
-    else if (resultYear == 0 && resultDay <= 0 && resultMonth){
-        year.innerHTML = "You have <mark> " + resultYear + " </mark>years",
-        month.innerHTML = " and <mark> " + Math.abs(resultMonth - 1) + " </mark>months",
-        day.innerHTML = " and <mark> " + Math.abs(resultDay + 30) + " </mark>days.<br><ins>&copy; 2008-2018 EL-NEHRAWY front-end web designer.All Rights Reserved.</ins>";     
-                         
-             }
+        else if (resultYear === 0 && resultDay <= 0 && resultMonth) {
+        year.innerHTML = "You have <mark> " + resultYear + " </mark>years";
+        month.innerHTML = " and <mark> " + Math.abs(resultMonth - 1) + " </mark>months";
+        day.innerHTML = " and <mark> " + Math.abs(resultDay + 30) + " </mark>days.<br><ins>&copy; 2008-2018 EL-NEHRAWY front-end web designer.All Rights Reserved.</ins>";
+    }
     // all values must be writen....................
-    else {
-        year.innerHTML = "You forget to write your age and date";  
-             }
-        
+        else {year.innerHTML = "You forget to write your age and date"; }
+    return x;
 }
-
+var x = age;
